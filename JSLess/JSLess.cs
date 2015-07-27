bc305b9cf5834c7f8aba5e8ad7801367acb82999
realstrategos <realstrategos@gmail.com>
2015-07-27 16:22:53 -0400

    v2.2.8 ToJsonObject now outputs readable json

--- JSLess/JSLess.cs
+++ JSLess/JSLess.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Text;
 using System.Web;
 using System.Web.Helpers;
 
@@ -8,11 +11,30 @@ namespace JSLess
 {
     public static class JsonExtensions
     {
-        public static string ToJsonObject<T>(this T source)
-        {            
-            var ret = Json.Encode(source);
-            return ret;
+        //public static string ToJsonObject<T>(this T source)
+        //{            
+        //    var ret = Json.Encode(source);
+        //    return ret;
+        //}
+        public static HtmlString ToJsonObject<T>(this T source)
+        {
+            StringBuilder sb = new StringBuilder();
+            using (StringWriter sw = new StringWriter(sb))
+            {
+                using (JsonTextWriter writer = new JsonTextWriter(sw))
+                {
+                    writer.QuoteChar = '\'';
+                    writer.QuoteName = false;
+                    writer.StringEscapeHandling = StringEscapeHandling.EscapeHtml;
 
+                    JsonSerializer ser = new JsonSerializer();
+                    ser.Serialize(writer, source);
+                }
+            }
+            var json = sb.ToString();
+            return new HtmlString(json);
+            //return json;
+            //return new HtmlString("JSON.parse('" + json + "')");
         }
 
     }
